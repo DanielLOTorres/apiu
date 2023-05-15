d34f1b8b7653bb6d2245dd0041c1cfdac93f46ff
Daniel Torres <daniellourenco30@gmail.com>
2023-05-15 02:47:23 -0300

    aaaaa

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express  from "express";
+const cron = require('node-cron');
 
 const app = express()
 const port = 9000
+const url = "https://gift-repo-eurawdqtt-daniellotorres.vercel.app/razoes"
+
+const randomTime = () => {
+    const hours = Math.floor(Math.random() * 24);
+    const minutes = Math.floor(Math.random() * 60);
+    return `${minutes} ${hours} * * *`;
+  };
+
 
 app.use("/", (req, res) => {
-    res.json({message : "Hello world"})
+    res.json({message : "Hello world",
+'randomTime': {randomTime}})
 })
 
 app.listen(9000, () =>{
