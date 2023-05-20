d4c7be39265dae85b5ffc97afea5e2288398240d
Daniel Torres <daniellourenco30@gmail.com>
2023-05-19 22:14:19 -0300

    ssssssssssss sssss

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ import cors from 'cors'
 const rts = cors
 const app = express();
 const port = 9000;
-const url = "https://gift-repo-eurawdqtt-daniellotorres.vercel.app/razoes";
+const url = "https://gift-repo.vercel.app/noticias";
 var id = 0;
 let usedIds = [];
 let data = {}
 
 const geraId = () => { 
-  if(usedIds.length < 12){
-    id = Math.floor(Math.random() * 12); 
+  if(usedIds.length < 7){
+    id = Math.floor(Math.random() * 6); 
     id === 0 ? id++ : id
     while (usedIds.indexOf(id) !== -1) {
-      id = Math.floor(Math.random() * 12);
+      id = Math.floor(Math.random() * 7);
       id === 0 ? id++ : id
     }
     usedIds.push(id);
     return id
   }
+  return id
 };
 
 const buscaDados = async() => {
@@ -36,18 +37,21 @@ const buscaDados = async() => {
 
 const montaMensagem = (dadoBruto) =>{
   return {
-    Num: dadoBruto.id,
-    Reason: dadoBruto.texto
+    Ordem: dadoBruto.id,
+    Noticia: dadoBruto.texto
   };
 }
 
-async function alteraRazao() {
-  geraId()
-  data = montaMensagem(await buscaDados())
-  console.log(data)
+async function alteraNoticia() {
+  let dadoBruto = await buscaDados(geraId())
+  data = montaMensagem(dadoBruto)
 }
 
-setInterval(alteraRazao, 60 * 1000);
+// setInterval(alteranoticia,  24 * 60 * 60 * 1000);
+
+
+
+setInterval(alteraNoticia, 3 * 60 * 1000);
 
 
 app.use(rts())
@@ -57,8 +61,13 @@ app.get("/", async (req, res) => {
 });
 
 app.post('/reset', (req, res) => {
-  id=0
+  usedIds = []
   res.json({message: 'Indice resetado'})
+})
+
+app.post('/change', (req, res) =>{
+  alteraNoticia()
+  res.json({message: 'Indice trocado'})
 })
 
 app.listen(9000, () => {
